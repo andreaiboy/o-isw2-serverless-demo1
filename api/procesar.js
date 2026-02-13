eb42818d8bbe439267498f5a8d15ba34329c533b
andreaiboy <123587381+andreaiboy@users.noreply.github.com>
2026-02-12 23:25:40 -0600

    Reto 05

--- api/procesar.js
+++ api/procesar.js
@@ -1,6 +1,11 @@
 export default function handler(req, res) {
   const nombre = req.query.nombre || "anónimo";
 
+  if (nombre === "error") {
+    return res.status(500).json({
+      error: "Falla simulada"
+    });
+  }
   res.status(200).json({
     resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
     longitud: nombre.length
